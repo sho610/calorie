483c911c8ebf6618567bddc6043b3f1576c1c192
sho610 <s3uzuy@gmail.com>
2022-02-21 04:23:40 +0000

    topページ

--- app/models/calender.rb
+++ app/models/calender.rb
@@ -1,2 +1,7 @@
 class Calender < ApplicationRecord
+  belongs_to :user
+
+  validates :title , presence: true
+  validates :content , presence: true
+  validates :start_time , presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'calenders#index'
+  root to: 'homes#top'
+  get "homes/top" => "homes#top"
   resources :calenders
 end
